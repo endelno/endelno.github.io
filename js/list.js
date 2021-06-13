b36a924807a482200300d8a2821679e874d830b6
Pandicon <mkpandaalt1@gmail.com>
2021-06-13 21:43:08 +0200

    First commit

---
+++ js/list.js
@@ -0,0 +1,497 @@
+const d = {
+  "list": [
+    /*=============================================================================*/
+    {
+			"vids": [
+         {
+           "user": "Nezzi",
+           "link": "https://www.youtube.com/watch?v=uqC_gqI44fo",
+           "percent": 100,
+           "hz": "240hz"
+         },
+         {
+           "user": "Pizza",
+           "link": "https://www.youtube.com/watch?v=vMdw-BddsRY",
+           "percent": 100,
+           "hz": "144hz"
+         },
+         {
+           "user": "Microwaveee",
+           "link": "https://www.youtube.com/watch?v=MFHSyHR7x9A",
+           "percent": 100,
+           "hz": "144hz"
+         },
+         {
+           "user": "Spaace",
+           "link": "https://www.youtube.com/watch?v=uSKg8VogQWM",
+           "percent": 100,
+           "hz": "144hz"
+         },
+],
+			"name": "Mistake",
+			"author": "Bluestone",
+			"id": 70098321,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/IiujgxpdqZE",
+			"key": 0
+		},
+    /*=============================================================================*/
+    {
+			"vids": [
+         {
+           "user": "Nezzi",
+           "link": "https://www.youtube.com/watch?v=libQPLWbtS8",
+           "percent": 100,
+           "hz": "240hz"
+         },
+         {
+           "user": "Luke",
+           "link": "https://www.youtube.com/watch?v=DJBA4xKGjhw",
+           "percent": 100,
+           "hz": "60hz"
+         },
+         {
+           "user": "hexuri",
+           "link": "https://www.youtube.com/watch?v=C5BQyYVJ8h4",
+           "percent": 100,
+           "hz": "180hz"
+         },
+],
+			"name": "Sunkist Berry Lemon",
+			"author": "Pandaa",
+			"id": 70084214,
+			"pass": "8310",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=66z7yQQEl04",
+			"key": 1
+		},
+    /*=============================================================================*/
+      {
+			"vids": [
+         {
+           "user": "Nezzi",
+           "link": "https://www.youtube.com/watch?v=fhtxpQt_qwE",
+           "percent": 100,
+           "hz": "240hz"
+         },
+         {
+           "user": "Luna101",
+           "link": "https://streamable.com/iht88k",
+           "percent": 100,
+           "hz": "144hz"
+         },
+],
+			"name": "WhaaaAaaaAAAAAaaaaTt",
+			"author": "Pandaa",
+			"id": 70095302,
+			"pass": "8310",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=fg5rFXvUVRc",
+			"key": 2
+		},
+    /*=============================================================================*/
+      {
+			"vids": [
+         {
+           "user": "Pandaa",
+           "link": "https://youtu.be/5bW1Q5tGLVs",
+           "percent": 100,
+           "hz": "300hz"
+         },
+         {
+           "user": "Nezzi",
+           "link": "https://www.youtube.com/watch?v=ZphPkO8Lrjo",
+           "percent": 100,
+           "hz": "240hz"
+         },
+         {
+           "user": "Microwaveee",
+           "link": "https://youtu.be/C3blu_0CcKM",
+           "percent": 100,
+           "hz": "144hz"
+         },
+         {
+           "user": "Spaace",
+           "link": "https://www.youtube.com/watch?v=9gmGf6ez4bg",
+           "percent": 100,
+           "hz": "144hz"
+         },
+],
+			"name": "Glory",
+			"author": "Bluestone",
+			"id": 70118680,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=ZphPkO8Lrjo",
+			"key": 3
+		},
+    /*=============================================================================*/
+      {
+			"vids": [
+         
+],
+			"name": "Uncanny",
+			"author": "Pandaa",
+			"id": 70120671,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/SEZHJLnv9r4",
+			"key": 4
+		},
+    /*=============================================================================*/
+      {
+			"vids": [
+         {
+           "user": "Spaace",
+           "link": "https://www.youtube.com/watch?v=eDK9_ZjxcQ4",
+           "percent": 100,
+           "hz": "144hz"
+         },
+         {
+           "user": "Pandaa",
+           "link": "https://youtu.be/niqy3U3TkAQ",
+           "percent": 100,
+           "hz": "300hz"
+         },
+         {
+           "user": "Scarlett",
+           "link": "https://www.youtube.com/watch?v=rme1JGNs4x8",
+           "percent": 100,
+           "hz": "Mobile"
+         },
+],
+			"name": "NSLCF",
+			"author": "Microwaveee",
+			"id": 70152323,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/rphkSYYsPFo",
+			"key": 5
+		},
+    /*=============================================================================*/
+      {
+			"vids": [
+         {
+           "user": "Pandaa",
+           "link": "https://youtu.be/bOUdTZa0HCI",
+           "percent": 100,
+           "hz": "300hz"
+         },
+],
+			"name": "Femboy Virus VI",
+			"author": "Spaace",
+			"id": 70114118,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=aSG7ZVUJsMw",
+			"key": 6
+		},
+    /*=============================================================================*/
+
+      {
+			"vids": [
+         {
+           "user": "Pandaa",
+           "link": "https://youtu.be/xt4fUoT8Tws",
+           "percent": 100,
+           "hz": "303hz"
+         },
+         {
+           "user": "hexuri",
+           "link": "https://www.youtube.com/watch?v=C5BQyYVJ8h4",
+           "percent": 100,
+           "hz": "240hz"
+         },
+    ],
+			"name": "Femboy Virus IV",
+			"author": "Spaace",
+			"id": 70082005,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=42GGjk1Q2HI",
+			"key": 7
+		},
+
+    /*=============================================================================*/
+      
+      {
+			"vids": [
+         {
+           "user": "Pandaa",
+           "link": "https://youtu.be/niqy3U3TkAQ",
+           "percent": 100,
+           "hz": "300hz"
+         },
+    ],
+			"name": "Femboy Virus VIII",
+			"author": "Spaace",
+			"id": 70151295,
+			"pass": "8310",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=7XKpjD-u9pg",
+			"key": 8
+		},
+    /*=============================================================================*/
+      
+      {
+			"vids": [
+         {
+           "user": "Spaace",
+           "link": "https://www.youtube.com/watch?v=Zc9KSwQc0eE",
+           "percent": 100,
+           "hz": "144hz"
+         }, 
+    ],
+			"name": "Femboy Blood",
+			"author": "Pandaa",
+			"id": 70109882,
+			"pass": "8310",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/RMqCogRX_Ik",
+			"key": 9
+		},
+    /*=============================================================================*/
+
+      {
+			"vids": [
+         {
+           "user": "Spaace",
+           "link": "https://www.youtube.com/watch?v=INWqYEdbrvs",
+           "percent": 100,
+           "hz": "144hz"
+         },
+    ],
+			"name": "Gynophobia",
+			"author": "Pandaa",
+			"id": 70115382,
+			"pass": "8310",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=ysXUdKWHe80",
+			"key": 10
+		},
+
+   /*=============================================================================*/
+
+      {
+			"vids": [
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/xt4fUoT8Tws",
+          "percent": 100,
+          "hz": "300hz"
+         },
+          {
+          "user": "Luna101",
+          "link": "https://streamable.com/ln7wfr",
+          "percent": 100,
+          "hz": "300hz"
+         },
+    ],
+			"name": "Femboy Virus VII",
+			"author": "Spaace",
+			"id": 70121549,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=6FsRScRWs3U",
+			"key": 11
+		},
+
+   /*=============================================================================*/
+
+      {
+			"vids": [
+    
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/2rOAncY8qAE",
+          "percent": 100,
+          "hz": "300hz"
+         },
+         {
+          "user": "Luke",
+          "link": "https://youtu.be/woPdLb_LguA",
+          "percent": 100,
+          "hz": "60hz"
+         },
+         {
+          "user": "hexuri",
+          "link": "https://www.youtube.com/watch?v=C5BQyYVJ8h4",
+          "percent": 100,
+          "hz": "240hz"
+         },
+
+    ],
+			"name": "Femboy Virus",
+			"author": "Spaace",
+			"id": 70074440,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=rIGyc_FuUGg",
+			"key": 12
+		},
+
+   /*======================================================================*/
+
+      {
+			"vids": [
+    
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/yrNamuiB_ck",
+          "percent": 100,
+          "hz": "300hz"
+         },
+         {
+          "user": "Luna101",
+          "link": "https://streamable.com/ln7wfr",
+          "percent": 100,
+          "hz": "144hz"
+         },
+    ],
+			"name": "Lucas Mineral",
+			"author": "Luke",
+			"id": 70087234,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/B2hRQu3aqCs",
+			"key": 13
+		},
+
+   /*=============================================================================*/
+      {
+			"vids": [
+    
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/1086PRcg7iY",
+          "percent": 100,
+          "hz": "300hz"
+         },
+         {
+          "user": "Luna101",
+          "link": "Femboy Virus V",
+          "percent": 100,
+          "hz": "144hz"
+         },
+         {
+          "user": "Scarlett",
+          "link": "https://www.youtube.com/watch?v=zpfebcAvnzU",
+          "percent": 100,
+          "hz": "Mobile"
+         },
+         {
+          "user": "Doodle",
+          "link": "https://www.youtube.com/watch?v=bDk6f0vtThA",
+          "percent": 100,
+          "hz": "75hz"
+         },
+
+    ],
+			"name": "Femboy Virus V",
+			"author": "Spaace",
+			"id": 70109327,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://youtu.be/1086PRcg7iY",
+			"key": 14
+		},
+
+   /*=============================================================================*/
+      {
+			"vids": [
+    
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/6aTweAGqoiA",
+          "percent": 100,
+          "hz": "300hz"
+         },
+         {
+          "user": "lboke",
+          "link": "https://youtu.be/qdUKK-Aub28",
+          "percent": 100,
+          "hz": "300hz"
+         }, 
+         {
+          "user": "Luke",
+          "link": "https://www.youtube.com/watch?v=zxeSTgIZ_cc",
+          "percent": 100,
+          "hz": "60hz"
+         }, 
+         {
+          "user": "Scarlett",
+          "link": "https://www.youtube.com/watch?v=R6OsGHxioCo",
+          "percent": 100,
+          "hz": "Mobile"
+         },
+         {
+          "user": "hexuri",
+          "link": "https://www.youtube.com/watch?v=C5BQyYVJ8h4",
+          "percent": 100,
+          "hz": "240hz"
+         },                
+
+    ],
+			"name": "Femboy Virus II",
+			"author": "Spaace",
+			"id": 70076853,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=y3RIksjJch4",
+			"key": 15
+		},
+
+   /*=============================================================================*/
+      {
+			"vids": [
+    
+        
+         {
+          "user": "Pandaa",
+          "link": "https://youtu.be/6aTweAGqoiA",
+          "percent": 100,
+          "hz": "300hz"
+         },
+         {
+          "user": "Luke",
+          "link": "https://youtu.be/laYNY5vxXpQ",
+          "percent": 100,
+          "hz": "60hz"
+         },
+         {
+          "user": "hexuri",
+          "link": "https://www.youtube.com/watch?v=C5BQyYVJ8h4",
+          "percent": 100,
+          "hz": "240hz"
+         },
+         {
+          "user": "mkpanda",
+          "link": "https://youtu.be/6_lTFL_vV3k",
+          "percent": 100,
+          "hz": "mobile"
+         },
+         
+         
+         
+
+    ],
+			"name": "Femboy Virus III",
+			"author": "Spaace",
+			"id": 70080610,
+			"pass": "No Pass",
+			"percentToQualify": 100,
+			"verificationVid": "https://www.youtube.com/watch?v=zNbOrtbf1eo",
+			"key": 16
+		},
+
+  ],
+   /*=============================================================================*/
+    "version": [
+      1,
+      0,
+      0
+    ],
+
+};
+const list = d.list; const version = d.version; { } 
